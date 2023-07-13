0dee467dbc26784bc90adaf96c8cbcea8e96024d
dondonson <137825460+dondonson@users.noreply.github.com>
2023-07-13 11:46:21 +0200

    Update DreieckLast.java

--- src/DreieckLast.java
+++ src/DreieckLast.java
@@ -1,28 +1,68 @@
+import inf.v3d.obj.Arrow;
+import inf.v3d.obj.Cylinder;
+import inf.v3d.obj.Text;
+import inf.v3d.view.Viewer;
+
 public class DreieckLast {
     //Attribute
-    double leange;
+    private double anfangspunkt;
+    private double endpunkt;
     double kraft;
     boolean ausrichtung;
 
     //Konstruktor
 
-    public DreieckLast(double l, double k, boolean a){
-        leange=l;
+    public DreieckLast(double ap,double ep, double k, boolean a){
+        anfangspunkt=ap;
+        endpunkt=ep;
         kraft=k;
         ausrichtung=a;
     }
     //Methoden
+    public double berechnelange(){
+        return endpunkt-anfangspunkt;
+    }
     public double BerechneResultierendeStandort(){
         if(ausrichtung = true){
-            return (leange) * 2 / 3;
+            return (berechnelange()) * 2 / 3;
         } else {
-            return (leange) * 1 / 3;
+            return (berechnelange()) * 1 / 3;
         }
     }
 
     public double BerechneResultierende(){
-        return leange*0.5*kraft;
+        return berechnelange()*0.5*kraft;
     }
-
+    //Visualisierung
+    //noch nocht ganz ausgereift
+    public void zu3D(Viewer v){
+    if (ausrichtung=true){
+        Cylinder c1 = new Cylinder(anfangspunkt,0,0,endpunkt,4,0);
+        Arrow a1 = new Arrow(endpunkt,4,0,endpunkt,0.1,0);
+        v.addObject3D(a1);
+        a1.setColor("red");
+        a1.setRadius(0.2);
+        v.addObject3D(c1);
+        c1.setColor("red");
+        c1.setRadius(0.2);
+    } else {
+        Cylinder c1 = new Cylinder(anfangspunkt,4,0,endpunkt,0,0);
+        Arrow a1 = new Arrow(anfangspunkt,4,0,anfangspunkt,0.1,0);
+        v.addObject3D(a1);
+        a1.setColor("red");
+        a1.setRadius(0.2);
+        v.addObject3D(c1);
+        c1.setColor("red");
+        c1.setRadius(0.2);
+    }
+        Arrow a2 = new Arrow(berechnelange()/2+anfangspunkt,2,0,berechnelange()/2+anfangspunkt,0.1,0);
+        v.addObject3D(a2);
+        a2.setColor("red");
+        a2.setRadius(0.2);
+        Text t1= new Text(""+kraft);
+        v.addObject3D(t1);
+        t1.setColor("red");
+        t1.setOrigin(endpunkt+1,4,0);
+    }
 
 }
