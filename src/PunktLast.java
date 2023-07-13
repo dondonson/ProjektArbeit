31ba91869add7c402b0e57af75cb461e5595a677
dondonson <137825460+dondonson@users.noreply.github.com>
2023-07-13 11:44:08 +0200

    Update PunktLast.java

--- src/PunktLast.java
+++ src/PunktLast.java
@@ -1,3 +1,8 @@
+import inf.v3d.obj.*;
+import inf.v3d.obj.Arrow;
+import inf.v3d.view.*;
+
+
 public class PunktLast {
     //Attribute
     private double ort;
@@ -24,5 +29,17 @@ public class PunktLast {
 
     public void setKraft(double kraft) {
         this.kraft = kraft;
+    }
+
+    //Visualiesierug der Last
+    public void zu3D(Viewer v){
+        Arrow a1 = new Arrow(ort,4,0,ort,0.1,0);
+        a1.setColor("red");
+        v.addObject3D(a1);
+        a1.setRadius(0.2);
+        Text t1= new Text(""+kraft);
+        v.addObject3D(t1);
+        t1.setColor("red");
+        t1.setOrigin(ort+1,4,0);
     }
 }
