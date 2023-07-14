4cc5a0c061541ec6f07cccc8befcaf7254af3320
dondonson <137825460+dondonson@users.noreply.github.com>
2023-07-14 21:13:04 +0200

    Update PunktLast.java

--- src/PunktLast.java
+++ src/PunktLast.java
@@ -7,11 +7,15 @@ public class PunktLast {
     //Attribute
     private double ort;
     private double kraft;
+    private Balken balken;
+    private int menge;
 
     //Konstruktor
-    public PunktLast(double o,double k){
+    public PunktLast(double o,double k, Balken b,int m){
         ort=o;
         kraft=k;
+        balken=b;
+        menge=m;
     }
 
     //Getter und Setter
@@ -30,16 +34,27 @@ public class PunktLast {
     public void setKraft(double kraft) {
         this.kraft = kraft;
     }
+    /*Work in Progress
+    public double berechneResultierendeOrt(){
+        double resultierendeOrt;
+        if (menge == 1) {
+            resultierendeOrt = ort;
+        } else {
+            resultierendeOrt = p[0].getOrt() + (p[1].getKraft() / resultierendeKraft) * (p[1].getOrt() - p[0].getOrt());
+        }
+        return resultierendeOrt;
+    }*/
 
     //Visualiesierug der Last
     public void zu3D(Viewer v){
-        Arrow a1 = new Arrow(ort,4,0,ort,0.1,0);
-        a1.setColor("red");
-        v.addObject3D(a1);
-        a1.setRadius(0.2);
-        Text t1= new Text(""+kraft);
-        v.addObject3D(t1);
-        t1.setColor("red");
-        t1.setOrigin(ort+1,4,0);
+        Arrow a1 = new Arrow(ort, balken.laenge / 3, 0, ort, 0.01* balken.laenge, 0);
+                a1.setColor("red");
+                v.addObject3D(a1);
+                a1.setRadius(0.01 * balken.laenge);
+                Text t1 = new Text("" + kraft);
+                v.addObject3D(t1);
+                t1.setColor("red");
+                t1.setOrigin(ort + 0.1, balken.laenge / 3, 0);
+                t1.setHeight(0.075 * balken.laenge);
     }
 }
