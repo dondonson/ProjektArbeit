ea3434f7f3e1ef5855db9174d07e16872f087bef
dondonson <137825460+dondonson@users.noreply.github.com>
2023-07-14 21:12:06 +0200

    Update DreieckLast.java

--- src/DreieckLast.java
+++ src/DreieckLast.java
@@ -1,68 +1,51 @@
-import inf.v3d.obj.Arrow;
-import inf.v3d.obj.Cylinder;
-import inf.v3d.obj.Text;
-import inf.v3d.view.Viewer;
-
-public class DreieckLast {
-    //Attribute
-    private double anfangspunkt;
-    private double endpunkt;
-    double kraft;
-    boolean ausrichtung;
-
-    //Konstruktor
-
-    public DreieckLast(double ap,double ep, double k, boolean a){
-        anfangspunkt=ap;
-        endpunkt=ep;
-        kraft=k;
-        ausrichtung=a;
+   public double berechneAuflagerkraft1(){
+        double ak1= BerechneResultierendeStandort() / balken.laenge * berechneResultierende();
+        return ak1;
     }
-    //Methoden
-    public double berechnelange(){
-        return endpunkt-anfangspunkt;
-    }
-    public double BerechneResultierendeStandort(){
-        if(ausrichtung = true){
-            return (berechnelange()) * 2 / 3;
-        } else {
-            return (berechnelange()) * 1 / 3;
-        }
-    }
-
-    public double BerechneResultierende(){
-        return berechnelange()*0.5*kraft;
+    public double berechneAuflagerkraft2(){
+        double ak2= berechneResultierende() - berechneAuflagerkraft1();
+        return ak2;
     }
     //Visualisierung
-    //noch nocht ganz ausgereift
-    public void zu3D(Viewer v){
-    if (ausrichtung=true){
-        Cylinder c1 = new Cylinder(anfangspunkt,0,0,endpunkt,4,0);
-        Arrow a1 = new Arrow(endpunkt,4,0,endpunkt,0.1,0);
-        v.addObject3D(a1);
-        a1.setColor("red");
-        a1.setRadius(0.2);
-        v.addObject3D(c1);
-        c1.setColor("red");
-        c1.setRadius(0.2);
+
+
+   public void zu3D(Viewer v){
+         Cylinder c1;
+         Arrow a1;
+
+    if (ausrichtung) {
+         c1 = new Cylinder(anfangspunkt, 0, 0, endpunkt, balken.laenge/3, 0);
+         a1 = new Arrow(endpunkt, balken.laenge/3, 0, endpunkt, 0.01* balken.laenge, 0);
+
     } else {
-        Cylinder c1 = new Cylinder(anfangspunkt,4,0,endpunkt,0,0);
-        Arrow a1 = new Arrow(anfangspunkt,4,0,anfangspunkt,0.1,0);
-        v.addObject3D(a1);
-        a1.setColor("red");
-        a1.setRadius(0.2);
-        v.addObject3D(c1);
-        c1.setColor("red");
-        c1.setRadius(0.2);
+         c1 = new Cylinder(anfangspunkt, balken.laenge/3, 0, endpunkt, 0, 0);
+         a1 = new Arrow(anfangspunkt, balken.laenge/3, 0, anfangspunkt, 0.01* balken.laenge, 0);
     }
-        Arrow a2 = new Arrow(berechnelange()/2+anfangspunkt,2,0,berechnelange()/2+anfangspunkt,0.1,0);
-        v.addObject3D(a2);
-        a2.setColor("red");
-        a2.setRadius(0.2);
-        Text t1= new Text(""+kraft);
-        v.addObject3D(t1);
-        t1.setColor("red");
-        t1.setOrigin(endpunkt+1,4,0);
-    }
+       v.addObject3D(a1);
+       a1.setColor("red");
+       a1.setRadius(0.01* balken.laenge);
+       v.addObject3D(c1);
+       c1.setColor("red");
+       c1.setRadius(0.01* balken.laenge);
+       Arrow a2 = new Arrow(berechnelange() / 2 + anfangspunkt, balken.laenge/6, 0, berechnelange() / 2 + anfangspunkt, 0.01* balken.laenge, 0);
+            v.addObject3D(a2);
+            a2.setColor("red");
+            a2.setRadius(0.01* balken.laenge);
+            Text t1 = new Text("" + kraft);
+            v.addObject3D(t1);
+            t1.setColor("red");
+            t1.setOrigin(endpunkt + 0.1, balken.laenge/3, 0);
+            t1.setHeight(0.075 * balken.laenge);
+       Text tak1 = new Text("" + berechneAuflagerkraft1());
+       v.addObject3D(tak1);
+       tak1.setColor("blue");
+       tak1.setOrigin(0.1, -balken.laenge / 3, 0);
+       Text tak2 = new Text("" + berechneAuflagerkraft2());
+       v.addObject3D(tak2);
+       tak2.setColor("blue");
+       tak2.setOrigin(balken.laenge + 0.1, -balken.laenge / 3, 0);
+       tak1.setHeight(0.075 * balken.laenge);
+       tak2.setHeight(0.075 * balken.laenge);
+            }
 
 }
