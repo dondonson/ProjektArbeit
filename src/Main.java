a3a261927447ad6478840b89fd98372cbd118399
Cedric <padeken.cedric@gmail.com>
2023-07-23 15:52:24 +0200

    So

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-import inf.v3d.obj.Arrow;
 import inf.v3d.obj.Polyline;
 import inf.v3d.obj.Text;
 import inf.v3d.view.*;
-import inf.v3d.obj.Polyline;
+
 import java.io.FileWriter;
 import java.io.PrintWriter;
 
@@ -14,8 +13,8 @@ public class Main {
         //Menü
         System.out.println("Bitte geben Sie alle ihre Angaben in Metern ein");
         //Erstellung vom Balken
-        Balken b1 = new Balken(Tastatur.liesDouble("Welche Größe hat der Balken ? "));
-        b1.zu3D(v);
+        Balken b = new Balken(Tastatur.liesDouble("Welche Größe hat der Balken ? "));
+        b.zu3D(v);
         //Auswahl
         System.out.println("Welche Art von Last wirkt auf dem Balken ?");
         System.out.println("Punktlast = 1");
@@ -41,7 +40,7 @@ public class Main {
             int mengePunktkraefte = Tastatur.liesInt("Wie viele Kräfte sollen Berechnet werden ? ");
             PunktLast[] p = new PunktLast[mengePunktkraefte];
             for (int i = 0; i < p.length; i++) {
-                p[i] = new PunktLast(Tastatur.liesDouble("Auf Welche länge des Balkens befindet sich die " + (i + 1) + " Punktlast ? "), Tastatur.liesDouble("Wie stark ist die Kraft der Last ? "),b1,mengePunktkraefte);
+                p[i] = new PunktLast(Tastatur.liesDouble("Auf Welche länge des Balkens befindet sich die " + (i + 1) + " Punktlast ? "), Tastatur.liesDouble("Wie stark ist die Kraft der Last ? "), b,mengePunktkraefte);
                 resultierendeKraft = resultierendeKraft + p[i].getKraft();
             }
 
@@ -55,12 +54,12 @@ public class Main {
             //Auflagerkräfte berechnen mit neuer Array
            
             for (int i = 0; i < p.length; i++) {
-                ak1 = ak1 + (b1.laenge-p[i].getOrt())*p[i].getKraft();
+                ak1 = ak1 + (b.laenge-p[i].getOrt())*p[i].getKraft();
                 ak2 = ak2 + p[i].getOrt()*p[i].getKraft();
             }
 
-            ak1 = ak1/ b1.laenge;
-            ak2 = ak2/ b1.laenge;
+            ak1 = ak1/ b.laenge;
+            ak2 = ak2/ b.laenge;
             System.out.println("Auflagerkraft 1 ist " + ak1 + " und Auflagerkraft 2 ist " + ak2);
 
             //Querkraft
@@ -79,29 +78,13 @@ public class Main {
                 drawquerkraftP.addVertex(p[i].getOrt(), qP[i],0);
             }
 
-            drawquerkraftP.addVertex(b1.laenge, ak1+x, 0);
-            drawquerkraftP.addVertex(b1.laenge, 0,0);
+            drawquerkraftP.addVertex(b.laenge, ak1+x, 0);
+            drawquerkraftP.addVertex(b.laenge, 0,0);
 
             drawquerkraftP.setLinewidth(5);
             drawquerkraftP.setVisible(true);
             drawquerkraftP.setColor("yellow");
             v.addObject3D(drawquerkraftP);
-
-            //Visualisierung
-            for (int i = 0;i<mengePunktkraefte;i++){
-                p[i].zu3D(v);
-            }
-
-            Text tak1 = new Text("" + ak1);
-            v.addObject3D(tak1);
-            tak1.setColor("blue");
-            tak1.setOrigin(0.1, -b1.laenge / 3, 0);
-            Text tak2 = new Text("" + ak2);
-            v.addObject3D(tak2);
-            tak2.setColor("blue");
-            tak2.setOrigin(b1.laenge + 0.1, -b1.laenge / 3, 0);
-            tak1.setHeight(0.075 * b1.laenge);
-            tak2.setHeight(0.075 * b1.laenge);
 
             //Momentverlauf
             Polyline line;
@@ -113,54 +96,44 @@ public class Main {
                 line.setLinewidth(3);
 
                 line.addVertex(0, 0, 0);
-                line.addVertex(p[0].getOrt(), (p[0].getKraft() * p[0].getOrt() * (b1.laenge - p[0].getOrt())) / b1.laenge, 0);
-                line.addVertex(b1.laenge, 0, 0);
+                line.addVertex(p[0].getOrt(), (p[0].getKraft() * p[0].getOrt() * (b.laenge - p[0].getOrt())) / b.laenge, 0);
+                line.addVertex(b.laenge, 0, 0);
             }else {
                 line.setLinewidth(4);
                 line.addVertex(0, 0, 0);
                 line.addVertex(p[0].getOrt(), 0, 0);
                 line.addVertex(p[1].getOrt(), 0, 0);
-                line.addVertex(b1.laenge, 0, 0);
+                line.addVertex(b.laenge, 0, 0);
             }
+
+            //Visualisierung
+            for (int i = 0;i<mengePunktkraefte;i++){
+                p[i].zu3D(v);
+            }
+
+            Text tak1 = new Text("" + ak1);
+            v.addObject3D(tak1);
+            tak1.setColor("blue");
+            tak1.setOrigin(0.1, -b.laenge / 3, 0);
+            Text tak2 = new Text("" + ak2);
+            v.addObject3D(tak2);
+            tak2.setColor("blue");
+            tak2.setOrigin(b.laenge + 0.1, -b.laenge / 3, 0);
+            tak1.setHeight(0.075 * b.laenge);
+            tak2.setHeight(0.075 * b.laenge);
+
         }
 
         //Gleichlast
         else if (n == 2) {
-            GleichLast g = new GleichLast(Tastatur.liesDouble("Auf Welche länge des Balkens beginnt die Gleichlast ? "), Tastatur.liesDouble("Auf Welche länge des Balkens endet die Gleichlast ? "), Tastatur.liesDouble("Wie stark ist die Kraft der Last ? "),b1);
+            GleichLast g = new GleichLast(Tastatur.liesDouble("Auf Welche länge des Balkens beginnt die Gleichlast ? "), Tastatur.liesDouble("Auf Welche länge des Balkens endet die Gleichlast ? "), Tastatur.liesDouble("Wie stark ist die Kraft der Last ? "), b);
              //Auflagerkräfte
             ak1=g.berechneAuflagerkraft1();
             ak2=g.berechneAuflagerkraft2();
             System.out.println("Auflagerkraft 1 ist " + ak1 + " und Auflagerkraft 2 ist " + ak2);
 
             //Querkraft
-            if (g.berechnelange() == 0) {
-                System.out.println("Die Querkraft Verläuft Linear durch die Mitte des Balkens und beträgt und startet bei +" + (g.getKraft() * b1.laenge) / 2 + " und endet bei -" + (g.getKraft() * b1.laenge) / 2);
-            } else if (g.berechnelange() != 0 && (g.getAnfangspunkt() == 0)) {
-                double A = (((g.getKraft() * Math.pow(g.berechnelange(), 2)) / (2 * b1.laenge)) - (g.getKraft() * g.berechnelange()));
-                double B = ((g.getKraft() * Math.pow(g.berechnelange(), 2)) / (2 * b1.laenge));
-                System.out.println("Die Querkraft fällt Linear von +" + A + " und bleibt dann bei " + B);
-            } else {
-                double A = (g.getKraft() * g.berechnelange()) / b1.laenge * (g.berechnelange() / 2 + (b1.laenge - g.getEndpunkt()));
-                double B = g.getKraft() * g.berechnelange() - A;
-                System.out.println("Die Querkraft beträgt bis " + g.getAnfangspunkt() + " " + A + "und fällt dann linear auf " + B + " am Punkt " + g.getEndpunkt() + " und bleibt stetig bis zum ende des Balkens");
-            }
-            //Momentverlauf
-            Polyline line;
-            line = new Polyline();
-            line.setVisible(true);
-            line.setColoringByData(true);
-            if (g.getAnfangspunkt() == 0&&g.getEndpunkt() ==b1.laenge) {
-                line.setLinewidth(10);
-                for (int i = 0; i < 6; i++) {
-                    line.addVertex((i / 10) * b1.laenge, (i / 5) * (g.getKraft() * Math.pow(b1.laenge, 2)) / 8, 0);
-                }
-                for (int i = 6; i < 11; i++) {
-                    line.addVertex((i / 10) * b1.laenge, (10 - i / 5) * (g.getKraft() * Math.pow(b1.laenge, 2)) / 8, 0);
-                }
-                //Visualisierung
-                g.zu3D(v);
-            }
-            double A = (g.getKraft()*g.berechnelange()/b1.laenge*(g.berechnelange()/2+(b1.laenge-g.getEndpunkt())));
+            double A = (g.getKraft()*g.berechnelange()/ b.laenge*(g.berechnelange()/2+(b.laenge-g.getEndpunkt())));
             double B = g.getKraft()*g.berechnelange()-A;
 
             Polyline querkraftG = new Polyline();
@@ -169,29 +142,66 @@ public class Main {
             querkraftG.addVertex(0, A, 0);
             querkraftG.addVertex(g.getAnfangspunkt(),A,0);
             querkraftG.addVertex(g.getEndpunkt(), -B,0);
-            querkraftG.addVertex(b1.laenge, -B, 0);
-            querkraftG.addVertex(b1.laenge, 0,0);
+            querkraftG.addVertex(b.laenge, -B, 0);
+            querkraftG.addVertex(b.laenge, 0,0);
 
             querkraftG.setLinewidth(5);
             querkraftG.setVisible(true);
-
             querkraftG.setColoringByData(true);
             v.addObject3D(querkraftG);
+
+            //Momentverlauf
+            Polyline line;
+            line = new Polyline();
+            line.setVisible(true);
+            line.setColoringByData(true);
+            if (g.getAnfangspunkt() == 0&&g.getEndpunkt() == b.laenge) {
+                line.setLinewidth(10);
+                for (int i = 0; i < 6; i++) {
+                    line.addVertex((i / 10) * b.laenge, (i / 5) * (g.getKraft() * Math.pow(b.laenge, 2)) / 8, 0);
+                }
+                for (int i = 6; i < 11; i++) {
+                    line.addVertex((i / 10) * b.laenge, (10 - i / 5) * (g.getKraft() * Math.pow(b.laenge, 2)) / 8, 0);
+                }
+                //Visualisierung
+                g.zu3D(v);
+            }
 
             //Visualisierung
             g.zu3D(v);
 
+         //DreieckLast
          } else if (n == 3) {
             System.out.println("Wird die Dreieckslast Größer oder Kleiner von links nach rechts? ");
             System.out.println("Größer = 1");
             System.out.println("Kleiner = 2");
-            DreieckLast d1 = new DreieckLast(Tastatur.liesInt(),Tastatur.liesDouble("Auf Welche länge des Balkens beginnt die Dreiecklast ? "), Tastatur.liesDouble("Auf Welche länge des Balkens endet die Dreiecklast ? "), Tastatur.liesDouble("Wie stark ist die Kraft der Last ? "),b1);
+            DreieckLast d = new DreieckLast(Tastatur.liesInt(),Tastatur.liesDouble("Auf Welche länge des Balkens beginnt die Dreiecklast ? "), Tastatur.liesDouble("Auf Welche länge des Balkens endet die Dreiecklast ? "), Tastatur.liesDouble("Wie stark ist die Kraft der Last ? "), b);
             //Auflagerkräfte
-            ak1 = d1.berechneAuflagerkraft1();
-            ak2 = d1.berechneAuflagerkraft2();
+            ak1 = d.berechneAuflagerkraft1();
+            ak2 = d.berechneAuflagerkraft2();
             System.out.println("Auflagerkraft 1 ist " + ak1 + " und Auflagerkraft 2 ist " + ak2);
+
+            //Querkraft
+            Polyline drawquerkraftD = new Polyline();
+
+            double A = (d.getKraft()/d.berechnelange())/2;
+
+            drawquerkraftD.addVertex(0,0,0);
+            drawquerkraftD.addVertex(0,ak1,0);
+            drawquerkraftD.addVertex(d.getAnfangspunkt(),ak1,0);
+            for (double i = 0; i <= d.berechnelange()+0.1;i = i+0.1){
+                drawquerkraftD.addVertex(d.getAnfangspunkt()+i, ak1 - A*Math.pow(i,2),0);
+            }
+            drawquerkraftD.addVertex(b.laenge, ak1 - A*Math.pow(d.berechnelange(), 2),0);
+            drawquerkraftD.addVertex(b.laenge, 0,0);
+
+            drawquerkraftD.setLinewidth(5);
+            drawquerkraftD.setVisible(true);
+            drawquerkraftD.setColoringByData(true);
+            v.addObject3D(drawquerkraftD);
+
             //Visualiesierung
-            d1.zu3D(v);
+            d.zu3D(v);
         }
         System.out.println("Wollen sie die Ergebnisse speichern?");
         System.out.println("Ja = 1");
@@ -207,7 +217,7 @@ public class Main {
             try {
                 FileWriter fw = new FileWriter("Balken.txt");
                 PrintWriter out = new PrintWriter(fw);
-                out.println("Balken der Lange " + b1.laenge);
+                out.println("Balken der Lange " + b.laenge);
                 out.println("Auflagerkraft 1 ist " + ak1 + " und Auflagerkraft 2 ist " + ak2);
                 out.close();
                 fw.close();
@@ -217,4 +227,4 @@ public class Main {
         }
         v.setVisible(true);
     }
-}}
+}
