e08b5bde0a8d5fae07ef5678e9a311f8f80d02c5
BauerBob69 <138206583+BauerBob69@users.noreply.github.com>
2023-07-25 16:49:40 +0200

    Update Balken.java

--- src/Balken.java
+++ src/Balken.java
@@ -1,7 +1,7 @@
 //Gruppe 109 
 //Cedric Padeken 108022252630
 //Henrique Elia Novais 108022232164
-//Leon Lukas Grügelsberg ´108022248789
+//Leon Lukas Grügelsberg 108022248789
 
 import inf.v3d.obj.*;
 import inf.v3d.view.*;
