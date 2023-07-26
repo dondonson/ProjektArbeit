d5026468b7b34f238ae71174c5fd68dac3f349ee
BauerBob69 <138206583+BauerBob69@users.noreply.github.com>
2023-07-26 15:16:55 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-
+         momentverlaufG.addVertex(g.getAnfangspunkt(), -1*Aa,0);
   for (double i = 0; i <= (g.berechnelange()*(1/2));i = i+0.1){
                 momentverlaufG.addVertex(i+g.getAnfangspunkt(), -1*(ak1*(i+g.getAnfangspunkt()) -(i*g.getKraft()*(i/2+g.getAnfangspunkt()))),0);
             }
             for (double i = 0 ; i <= (g.berechnelange()*(1/2));i = i+0.1){
                 momentverlaufG.addVertex(i+(g.berechnelange()*(1/2)+g.getAnfangspunkt()), -1*(ak2*(b.laenge-(i+g.berechnelange()*(1/2)+g.getAnfangspunkt())) -((g.getEndpunkt()-(i/2+g.berechnelange()*(1/2)+g.getAnfangspunkt()))*g.getKraft()*(b.laenge-(g.berechnelange()*(1/2)+g.getAnfangspunkt()+i/2)))),0);
             }
+momentverlaufG.addVertex(g.getEndpunkt(), -1*Bb,0);
