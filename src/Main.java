8b053df1a472b7d3dba5f379fea0db7fbec05c08
BauerBob69 <138206583+BauerBob69@users.noreply.github.com>
2023-07-26 19:50:11 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -34,3 +34,19 @@ momentverlaufG.addVertex(g.getEndpunkt(), -1*Bb,0);
       for (double i = 0; i <= g.berechnelange();i = i+0.1){
                 momentverlaufG.addVertex(i+Ap, -1*(ak1*(i+Ap)-(i/2+Ap)*g.getKraft()*i),0);
             }
+  double s1 = Ap/b.laenge;
+            double s2 = (b.laenge-Ep)/b.laenge;
+            double s3 = (g.berechnelange())/b.laenge;
+            double gamma = (s1+s3)/2;
+            double epsilon = 1- gamma;
+
+ for (double i = g.getAnfangspunkt(); i <= A/ g.getKraft()-0.1;i = i+0.1){
+                momentverlaufG.addVertex(i, -1*(ak1*i-(i- g.getAnfangspunkt())*g.getKraft()*((i-Ap)/2+Ap)),0);
+            }
+            momentverlaufG.addVertex(A/ g.getKraft(), -1*(1-s3/2)*gamma*epsilon*b.laenge* g.getKraft()*g.berechnelange(),0);
+            for (double i = A/ g.getKraft() ; i <= g.getEndpunkt();i = i+0.1){
+                momentverlaufG.addVertex(i, -1*(ak2*(b.laenge-i) -(b.laenge- g.getEndpunkt()+ (g.getEndpunkt()-i)/2)* (g.getEndpunkt()-i)* g.getKraft()),0);
+            }
+
+
+    
